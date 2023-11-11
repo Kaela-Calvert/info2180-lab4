df4737cfe7d374a7a56ecb6b04e8bb81c3642957
Kaela Calvert <kaelacalvert14@gmail.com>
2023-11-11 17:13:29 -0500

    Edited Exercise 2

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-window.onload=function(){
-
-    var searchBtn= document.querySelector('#search');
-    var httpRequest;
-
-    searchBtn.addEventListener("click",function(element){
-        element.preventDefault();
-        httpRequest = new XMLHttpRequest();
-        var url = "superheroes.php";
-        httpRequest.onreadystatechange = listheroes();
-        httpRequest.open('GET', url);
-        httpRequest.send();
-    });
-
-    function listheroes() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                var response = httpRequest.responseText;
-                var quote = document.querySelector("#quote");
-                quote.innerHTML=response;
-            } else {
-                alert('There was a problem with the request.');
-            }
-        }
-    }
-
-};
+function loadXMLDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("search").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "superheroes.php", true);
+    xhttp.send();
+  }
